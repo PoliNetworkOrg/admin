05aa2a9cd1501801b3aaddbedc46ee1f7adce88a
Lorenzo Corallo <lorenzo.corallo@outlook.it>
2025-04-15 00:05:07 +0200

    fix: rewrites in middleware

--- next.config.js
+++ next.config.js
@@ -2,19 +2,11 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-import { env } from "./src/env.js";
+import "./src/env.js";
 
 /** @type {import("next").NextConfig} */
 const config = {
   output: "standalone",
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: `${env.BACKEND_URL}/api/:path*`,
-      },
-    ];
-  },
 };
 
 export default config;
