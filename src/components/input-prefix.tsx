a796e5649a74b17c282143fe1585a8f2cad80503
Lorenzo <lorenzo.corallo@outlook.it>
2025-07-28 15:43:05 +0200

    fix(input-prefix): add container classname in props

--- src/components/input-prefix.tsx
+++ src/components/input-prefix.tsx
@@ -5,14 +5,15 @@ import { Input } from "./ui/input";
 export interface InputWithPrefixProps
   extends React.InputHTMLAttributes<HTMLInputElement> {
   prefix: string;
+  containerClassName?: string;
 }
 
 const InputWithPrefix = React.forwardRef<
   HTMLInputElement,
   InputWithPrefixProps
->(({ className, type, prefix, ...props }, ref) => {
+>(({ className, type, prefix, containerClassName, ...props }, ref) => {
   return (
-    <div className="relative rounded-md overflow-hidden">
+    <div className={cn("relative rounded-md overflow-hidden", containerClassName)}>
       <div className="text-accent-foreground/70 bg-accent/30 pointer-events-none absolute inset-y-0 left-0 flex items-center border-r border-input pr-2 pl-3">
         {prefix}
       </div>
