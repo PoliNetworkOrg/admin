0c45681362a7de62908ef30ecc960318599c1992
Lorenzo Corallo <lorenzo.corallo@outlook.it>
2025-12-25 15:19:40 +0100

    fix: react-compiler stable version, rename import in next config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next"
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-import "./src/env.js"
+import "./src/env.ts"
 
 /** @type {import("next").NextConfig} */
 const config: NextConfig = {
