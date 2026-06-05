ef6822600c87f6e76f314466ad4cbb77022bc8be
Lorenzo Corallo <lorenzo.corallo@outlook.it>
2026-06-05 18:54:59 +0200

    fix: add tg/grants and remove boilerplate

--- src/components/dashboard-sidebar/data.tsx
+++ src/components/dashboard-sidebar/data.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image"
+import azureSvg from "@/assets/svg/azure.svg"
 import telegramSvg from "@/assets/svg/telegram.svg"
+
 export type NavItem = {
   title: string
   url: string
@@ -13,144 +15,16 @@ export const DSData = {
       url: "/dashboard/telegram",
       icon: <Image alt="telegram logo" src={telegramSvg} className="size-4" />,
       items: [
+        { title: "Grants", url: "/dashboard/telegram/grants" },
         { title: "Groups", url: "/dashboard/telegram/groups" },
         { title: "Users", url: "/dashboard/telegram/user-list" },
       ],
     },
     {
-      title: "Getting Started",
-      url: "#",
-      items: [
-        {
-          title: "Installation",
-          url: "#",
-        },
-        {
-          title: "Project Structure",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Build Your Application",
-      url: "#",
-      items: [
-        {
-          title: "Routing",
-          url: "#",
-        },
-        {
-          title: "Data Fetching",
-          url: "#",
-          isActive: true,
-        },
-        {
-          title: "Rendering",
-          url: "#",
-        },
-        {
-          title: "Caching",
-          url: "#",
-        },
-        {
-          title: "Styling",
-          url: "#",
-        },
-        {
-          title: "Optimizing",
-          url: "#",
-        },
-        {
-          title: "Configuring",
-          url: "#",
-        },
-        {
-          title: "Testing",
-          url: "#",
-        },
-        {
-          title: "Authentication",
-          url: "#",
-        },
-        {
-          title: "Deploying",
-          url: "#",
-        },
-        {
-          title: "Upgrading",
-          url: "#",
-        },
-        {
-          title: "Examples",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "API Reference",
-      url: "#",
-      items: [
-        {
-          title: "Components",
-          url: "#",
-        },
-        {
-          title: "File Conventions",
-          url: "#",
-        },
-        {
-          title: "Functions",
-          url: "#",
-        },
-        {
-          title: "next.config.js Options",
-          url: "#",
-        },
-        {
-          title: "CLI",
-          url: "#",
-        },
-        {
-          title: "Edge Runtime",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Architecture",
-      url: "#",
-      items: [
-        {
-          title: "Accessibility",
-          url: "#",
-        },
-        {
-          title: "Fast Refresh",
-          url: "#",
-        },
-        {
-          title: "Next.js Compiler",
-          url: "#",
-        },
-        {
-          title: "Supported Browsers",
-          url: "#",
-        },
-        {
-          title: "Turbopack",
-          url: "#",
-        },
-      ],
-    },
-    {
-      title: "Community",
-      url: "#",
-      items: [
-        {
-          title: "Contribution Guide",
-          url: "#",
-        },
-      ],
+      title: "Azure",
+      url: "/dashboard/azure",
+      icon: <Image alt="azure logo" src={azureSvg} className="size-4" />,
+      items: [{ title: "Members", url: "/dashboard/azure/members" }],
     },
   ],
 }
