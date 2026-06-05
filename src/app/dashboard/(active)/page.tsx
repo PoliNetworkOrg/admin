fd76b346c5f7f44fd4ba57dc7366a604d7b708a8
Lorenzo Corallo <lorenzo.corallo@outlook.it>
2026-06-05 23:59:05 +0200

    fix: cleanup home

--- src/app/dashboard/(active)/page.tsx
+++ src/app/dashboard/(active)/page.tsx
@@ -1,8 +1,5 @@
-import Image from "next/image"
-import Link from "next/link"
-import azureSvg from "@/assets/svg/azure.svg"
-import telegramSvg from "@/assets/svg/telegram.svg"
-import { Card, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
+import { InfoIcon } from "lucide-react"
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
 import { getServerSession } from "@/server/auth"
 import { CompleteProfile } from "./complete-profile"
 
@@ -12,33 +9,11 @@ export default async function AdminHome() {
     session && (
       <div className="container">
         <CompleteProfile user={session.user} />
-        <h2 className="text-accent-foreground mb-4 text-3xl font-bold">Home</h2>
-
-        <div className="gap-4 flex justify-start flex-wrap items-center">
-          <Link href="/dashboard/azure">
-            <Card className="w-90 hover:bg-accent transition-colors">
-              <CardHeader>
-                <CardTitle className="flex gap-2">
-                  <Image alt="azure logo" src={azureSvg} className="size-5" />
-                  Azure
-                </CardTitle>
-                <CardDescription>Manage Azure related things</CardDescription>
-              </CardHeader>
-            </Card>
-          </Link>
-
-          <Link href="/dashboard/telegram">
-            <Card className="w-90 hover:bg-accent transition-colors">
-              <CardHeader>
-                <CardTitle className="flex gap-2">
-                  <Image alt="telegram logo" src={telegramSvg} className="size-5" />
-                  Telegram
-                </CardTitle>
-                <CardDescription>Manage Telegram related things</CardDescription>
-              </CardHeader>
-            </Card>
-          </Link>
-        </div>
+        <Alert variant="info">
+          <InfoIcon />
+          <AlertTitle>Page under construction</AlertTitle>
+          <AlertDescription>Use the sidebar to access the sections</AlertDescription>
+        </Alert>
       </div>
     )
   )
--- src/components/ui/alert.tsx
+++ src/components/ui/alert.tsx
@@ -9,6 +9,7 @@ const alertVariants = cva(
     variants: {
       variant: {
         default: "bg-card text-card-foreground",
+        info: "bg-primary/20 text-primary-foreground",
         destructive:
           "bg-card text-destructive *:data-[slot=alert-description]:text-destructive/90 *:[svg]:text-current",
       },
