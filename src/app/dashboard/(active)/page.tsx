0b1596c723876ae73d2afcd3c4aa8e5f5887bc43
Lorenzo Corallo <lorenzo.corallo@outlook.it>
2026-06-06 20:45:38 +0200

    perf: make dashboard homepage static for now

--- src/app/dashboard/(active)/page.tsx
+++ src/app/dashboard/(active)/page.tsx
@@ -1,20 +1,16 @@
 import { InfoIcon } from "lucide-react"
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
-import { getServerSession } from "@/server/auth"
 import { CompleteProfile } from "./complete-profile"
 
-export default async function AdminHome() {
-  const { data: session } = await getServerSession()
+export default function AdminHome() {
   return (
-    session && (
-      <div className="container">
-        <CompleteProfile user={session.user} />
-        <Alert variant="info">
-          <InfoIcon />
-          <AlertTitle>Page under construction</AlertTitle>
-          <AlertDescription>Use the sidebar to access the sections</AlertDescription>
-        </Alert>
-      </div>
-    )
+    <div className="container">
+      <CompleteProfile />
+      <Alert variant="info">
+        <InfoIcon />
+        <AlertTitle>Page under construction</AlertTitle>
+        <AlertDescription>Use the sidebar to access the sections</AlertDescription>
+      </Alert>
+    </div>
   )
 }
