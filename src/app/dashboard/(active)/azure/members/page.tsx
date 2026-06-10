64453979f144bc5715b1b11510ceb073f7fd1205
Lorenzo Corallo <lorenzo.corallo@outlook.it>
2026-06-11 01:25:37 +0200

    chore: remove unused import

--- src/app/dashboard/(active)/azure/members/page.tsx
+++ src/app/dashboard/(active)/azure/members/page.tsx
@@ -1,13 +1,11 @@
 import { Suspense } from "react"
 import { ErrorBoundary } from "react-error-boundary"
 import { Spinner } from "@/components/spinner"
-import { wait } from "@/lib/utils"
 import { getAzureMembers } from "@/server/actions/azure"
 import { AssocTable } from "./table"
 
 export default async function AssocMembers() {
   const members = await getAzureMembers()
-  // await wait(120_000)
 
   return (
     <div className="container p-8">
